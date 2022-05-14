0e5b2af9f164f7690bf0b8a3cf4fab62f0626f1d
corentintran <102200377+corentintran@users.noreply.github.com>
2022-05-14 13:38:16 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# test
+# projectWebComputing
 Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Volcanoes of the world</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </header>
+    </div>
+  );
+}
