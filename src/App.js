8dcc5ae72595514cfa500aee017e514cbfca279f
corentintran <102200377+corentintran@users.noreply.github.com>
2022-05-14 15:45:19 +1000

    navig

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 //import React, { useState } from "react";
 import "./styles.css";
 
+import Navigation from "./navigation.js";
+
 export default function App() {
   return (
     <div className="App">
@@ -8,6 +10,8 @@ export default function App() {
         <h1>Volcanoes of the world</h1>
         <h2>Start editing to see some magic happen!</h2>
       </header>
+
+      <Navigation />
     </div>
   );
 }
---
+++ src/navigation.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import "./navigation.css";
+
+const Navigation = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Volcano list</a>
+        </li>
+        <li>
+          <a href="/">Register</a>
+        </li>
+        <li>
+          <a href="/">Login</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navigation;
